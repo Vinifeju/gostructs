efb8dfcdbaa294a342b2b441a72f71b96607f6ba
Vinifeju <larasik22@gmail.com>
2024-03-19 21:27:32 +0500

    sasdasd

---
+++ tests/bheap_test.go
@@ -0,0 +1,46 @@
+package tests
+
+import (
+	"gostructs"
+	"testing"
+)
+
+func heapClean(heap *gostructs.Bheap) {
+	heaplen := len(heap.HeapItems())
+	for i := 0; i <= heaplen; i++ {
+		heap.DelMin()
+	}
+}
+
+func TestDelAllItems(t *testing.T) {
+	heap := gostructs.InitHeap([]int{1, 9, 7, 4, 2, 3})
+
+	heapClean(&heap)
+
+	if len(heap.HeapItems()) != 0 {
+		t.Errorf("error TestDelAllItems got %v ", heap.HeapItems())
+	}
+
+	if heap.HeapLen() > 0 {
+		t.Errorf("error TestDelAllItems got %d ", heap.HeapLen())
+	}
+}
+
+func TestWithEmptyHeap(t *testing.T) {
+	heap := gostructs.InitHeap([]int{1, 9, 7, 4, 2, 3})
+	heapClean(&heap)
+	heap.Insert(10)
+
+	// empty heaplen is  -1
+	if heap.HeapLen() != 0 {
+		t.Errorf("Error heap insert got %v", heap.HeapItems())
+	}
+
+	if heap.HeapItems()[0] != 10 {
+		t.Errorf("Error heap insert got %v", heap.HeapItems())
+	}
+	heap.Insert(2)
+	if heap.HeapItems()[0] != 2 {
+		t.Errorf("Error heap insert got %v", heap.HeapItems())
+	}
+}
